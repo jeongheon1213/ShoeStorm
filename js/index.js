7878595a4e934514ee2e97ed420dcfac9c329c2f
dlwjdgjs1213 <alal02j@gmail.com>
2021-03-22 21:42:06 +0900

    정육각형 회전

--- js/index.js
+++ js/index.js
@@ -1,38 +1,42 @@
-// ~ 내코드
 import * as THREE from 'three';
-import DragControls from 'three-dragcontrols';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { DecalGeometry } from 'three/examples/jsm/geometries/DecalGeometry';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0xffffff  } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
 camera.position.z = 5;
-decalInit(THREE);
 
-const geometry2 =  THREE.DecalGeometry( cube, new THREE.Vector3( 0, 1, 0 ), new THREE.Euler(0,0,1,'XYZ'), new THREE.Vector3( 1, 1, 1 ) );
-const material2 = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const mesh2 = new THREE.Mesh( geometry2, material2 );
-scene.add( mesh2 );
+renderer.render(scene, camera);
+
+// const controls = new DragControls([cube], camera, renderer.domElement);
+
+// controls.addEventListener('drag', render);
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.rotateSpeed = 1.0;
+controls.zoomSpeed = 1.2;
+controls.minDistance = 5; // 마우스 휠로 카메라 거리 조작시 최소 값. 기본값(Float)은 0 입니다.
+controls.maxDistance = 100; // 마우스 휠로 카메라 거리 조작시 최대 값. 기본값(Float)은 무제한 입니다.
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    renderer.render(scene, camera);
+    controls.update(); // 마우스로인해 변경된 카메라값을 업데이트 합니다.
+}
+
+animate();
 
 
-renderer.render( scene, camera );
-
-
-const controls = new DragControls( [ cube ], camera, renderer.domElement );
-
-
-controls.addEventListener('drag', render);
-
-
-function render(event ) {
-    console.log(event.object.position);era );
-
+function render(event) {
+    renderer.render(scene, camera);
 }
